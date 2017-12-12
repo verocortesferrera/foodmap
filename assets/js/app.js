b6ba385fcd72d62820622164c0ba84e4aad25f07
verocortesferrera <verocortesferrera@gmail.com>
2017-12-12 11:34:18 -0300

    splash

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+/*boom*/
+$(document).ready(function() {
+/*splash*/
+	setTimeout(function() {
+		$(".content").fadeOut(500);
+		},3000);
+		setTimeout(function() {
+			$(".content2").fadeIn(500);
+		});
+
+
+
+
+});
